c5bb3320c6885693f457bf55b69c3e2d76186695
unknown <aguirrealexorri@gmail.com>
2019-05-21 22:08:56 -0500

    Pendientes

--- src/app/pendiente/pendiente.page.ts
+++ src/app/pendiente/pendiente.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { VentaOptions } from '../venta-options';
+import { EstadoVenta } from '../estado-venta.enum';
 
 @Component({
   selector: 'app-pendiente',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PendientePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private angularFirestore: AngularFirestore
+  ) { }
 
   ngOnInit() {
+  }
+
+  private loadVentasDia(id: string){
+    const ventasPendientesCollection = this.angularFirestore.collection<VentaOptions>(`ventas/${id}/ventas`, ref => ref.where('estado', '==', EstadoVenta.ENTREGADO));
+    ventasPendientesCollection.valueChanges().subscribe(ventas => {
+      ventas
+    });
+  }
+
+  public updatePendientes() {
+    const pendientesCollection = this.angularFirestore.collection<any>('ventas', ref => ref.where('pendientes', '>=', 1));
+    pendientesCollection.get().subscribe(pendientes => {
+      pendientes.forEach(dia => {
+        this.loadVentasDia(dia.id);
+      });
+    });
   }
 
 }
